e14516f17a584796936c9943d4b5a5300dfcb4c6
TatoGar12 <facunddogarcia@gmail.com>
2024-05-22 15:07:21 -0300

    Optimizacion del codigo, agregamos Ofertas.

--- WebPs.php
+++ WebPs.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="Styles/stylePs.css">
+    <link rel="stylesheet" href="Styles/stylePlataforma.css">
     <link rel="icon" href="Imagenes/3d.png">
     <title>PixelGames</title>
 </head>
